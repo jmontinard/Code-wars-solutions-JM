9848aea6c28e14680ca6dde29cfd5e5651e6f617
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2021-09-23 22:48:07 -0400

    Update index.js

--- code wars/index.js
+++ code wars/index.js
@@ -201,3 +201,4 @@ function solution(list){
  console.log( solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18,21, 19, 20]));
  // returns "-6,-3-1,3-5,7-11,14,15,17-20"
  
+//wow it worked
