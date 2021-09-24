885a172aac57044fb62b3f6acdf4d797d0af047c
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2021-09-23 22:49:45 -0400

    Update index.js
    Set objectives for tomorrow

--- code wars/index.js
+++ code wars/index.js
@@ -201,4 +201,4 @@ function solution(list){
  console.log( solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18,21, 19, 20]));
  // returns "-6,-3-1,3-5,7-11,14,15,17-20"
  
-//wow it worked
+// objective for sep 23 finish the level 4 kyu 95% done 
